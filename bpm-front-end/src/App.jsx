518cc739a48f4b9b1b155f233d226d0aa14f5c65
aadean11 <fiqridean@gmail.com>
2024-11-29 14:12:44 +0700

    iseng aja

--- bpm-front-end/src/App.jsx
+++ bpm-front-end/src/App.jsx
@@ -1,3 +1,20 @@
+<<<<<<< HEAD
+=======
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/backbone/Header'; 
+import Footer from './components/backbone/Footer';
+import Tentang from './components/page/2_Tentang/Root';
+import Berita from './components/page/3_Berita/Root';
+import RencanaKegiatan from './components/page/4_Kegiatan/Root'
+import Survei from './components/page/9_Survei/Root';
+import Pelaksanaan from './components/page/5_SPMI/siklus_spmi/pelaksanaan/Root';
+import Penetapan from './components/page/5_SPMI/siklus_spmi/penetapan/Root';
+import Peningkatan from './components/page/5_SPMI/siklus_spmi/peningkatan/Root';
+import Pengendalian from './components/page/5_SPMI/siklus_spmi/pengendalian/Root';
+import Evaluasi from './components/page/5_SPMI/siklus_spmi/evaluasi/Root';
+import "./App.css";
+>>>>>>> 04423573868fcf6aaaf6ff09f4246596e0bd3f60
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/backbone/Header";
 import Footer from "./components/backbone/Footer";
