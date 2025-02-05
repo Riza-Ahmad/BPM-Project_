fa8821e61a51945030e32832533a99cc49ffdfc0
halofahind <rizkyardhane01@gmail.com>
2025-02-05 16:31:40 +0700

    alalala

--- bpm-front-end/src/components/page/9_Survei/Template_Survei/Add.jsx
+++ bpm-front-end/src/components/page/9_Survei/Template_Survei/Add.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useEffect,useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import PageTitleNav from "../../../part/PageTitleNav";
 import HeaderForm from "../../../part/HeaderText";
